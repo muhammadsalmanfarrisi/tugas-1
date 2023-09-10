6306e8f82cd1a98281f016b740d75634bf7f25ba
SalmanFarrisi <muhammad.farrisi@mhs.unsoed.ac.id>
2023-09-10 22:56:09 +0700

    Update Yang benar

--- main.dart
+++ main.dart
@@ -6,10 +6,16 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya adalah $nama");
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $tahunLahir tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021088", "MUHAMMAD SALMAN FARRISI", 2003);
+  mhs.perkenalan();
+  //untuk menggunakan Clue: Untuk mengetahui tahun saat ini, gunakan `DateTime.now().year`
+  var sk =
+      Mahasiswa("H1D021120", "Muhammad Sakinah Wirdho", DateTime.now().year);
+  sk.perkenalan();
 }
